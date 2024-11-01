b2294751e72b21dc064172389cab4084f8464012
Rituraj <singhrituraj783@gmail.com>
2024-11-01 18:29:12 +0530

    Added layout and course creation UI

---
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "lucide-react";
+
+const CoursesPage = () => {
+    return ( 
+    <div>
+        <Link href="/teacher/create">
+        <Button className="p-6">
+            New Course
+        </Button>
+        </Link>    
+    </div> );
+}
+ 
+export default CoursesPage;
